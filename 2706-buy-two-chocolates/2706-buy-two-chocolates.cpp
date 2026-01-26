4bf374f39dd7792acaa54c372958816914ef36a9
Khushi Yadav <ky0307005@gmail.com>
2026-01-26 18:53:41 +0530

    Time: 0 ms (100%), Space: 52.3 MB (3.58%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(), prices.end());
-        int total = 0;
-
-        for(int i = 0; i<prices.size() - 1; i++){
-            if(prices[i] + prices[i+1] <= money){
-                return money - (prices[i] + prices[i+1]);
-            }
-        }
-        return money;
+        sort(prices.begin(),prices.end());
+        if((prices[0]+prices[1])>money) return money;
+        else return money-(prices[0]+prices[1]);
     }
 };
