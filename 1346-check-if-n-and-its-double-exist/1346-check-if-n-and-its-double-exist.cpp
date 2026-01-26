13ee67e878a528acd609ef6bec33305229885e50
Khushi Yadav <ky0307005@gmail.com>
2026-01-26 18:41:54 +0530

    Time: 0 ms (100%), Space: 13.6 MB (70.56%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,31 +1,14 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-
-        sort(arr.begin(), arr.end());
-        int n = arr.size();
-
-        for (int i = 0; i < n; i++) {
-
-            int target = 2 * arr[i];
-
-            int left = 0, right = n - 1;
-
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-
-                if (arr[mid] == target && mid != i) {
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr.size();j++){
+                if(i!=j && arr[i]==2*arr[j]){
                     return true;
-                }
-                else if (arr[mid] < target) {
-                    left = mid + 1;
-                }
-                else {
-                    right = mid - 1;
                 }
             }
         }
-
         return false;
+        
     }
 };
