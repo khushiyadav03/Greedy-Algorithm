ce194dfd5d50d3c9e88e5809dbf52acf4946536f
Khushi Yadav <ky0307005@gmail.com>
2026-01-29 01:24:52 +0530

    Time: 0 ms (100%), Space: 23 MB (53.38%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,23 +2,28 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
+
+        // initialize candies array with 1
         vector<int> arr(n, 1);
 
-        for(int i = 1; i<n; i++){
-            if(ratings[i] > ratings[i-1]){
+        // left to right pass
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
                 arr[i] = arr[i - 1] + 1;
             }
         }
 
-        for(int i = n - 2; i>=0; i--){
-            if(ratings[i] > ratings[i+1]){
+        // right to left pass
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
                 arr[i] = max(arr[i], arr[i + 1] + 1);
             }
         }
 
+        // sum all candies
         int sum = 0;
-        for(int a : arr){
-            sum += a;
+        for (int x : arr) {
+            sum += x;
         }
 
         return sum;
